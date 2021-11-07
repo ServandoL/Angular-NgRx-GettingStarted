bdbe766a69195a833466843227128b5d6e83ef97
Servando <swyswy214@gmail.com>
2021-11-07 10:05:01 -0600

    defining actions for complex operations

--- APM-Demo0/src/app/products/state/product.actions.ts
+++ APM-Demo0/src/app/products/state/product.actions.ts
@@ -1,9 +1,7 @@
-import { createAction, props } from "@ngrx/store";
-import { Product } from "../product";
+import { createAction, props } from '@ngrx/store';
+import { Product } from '../product';
 
-export const toggleProductCode = createAction(
-  '[Product] Toggle Product Code'
-);
+export const toggleProductCode = createAction('[Product] Toggle Product Code');
 
 export const setCurrentProduct = createAction(
   '[Product] Set CurrentProduct',
@@ -16,4 +14,16 @@ export const clearCurrentProduct = createAction(
 
 export const initializeCurrentProduct = createAction(
   '[Product] Initialize Current Product'
+);
+
+export const loadProducts = createAction('[Product] Load');
+
+export const loadProductsSuccess = createAction(
+  '[Product] Load Success',
+  props<{ products: Product[] }>()
+);
+
+export const loadProductsFailure = createAction(
+  '[Product] Load Failure',
+  props<{ error: string }>()
 );
